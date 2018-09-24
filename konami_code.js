366e911bd83a2e16d56f6a2489b11813cbce3f71
Gabrielle Bellini <gbellini90@gmail.com>
2018-09-24 16:48:57 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -14,26 +14,27 @@ const codes = [
 let index = 0;
 
 function init() {
+
 document.addEventListener('keydown', function (event) {
-const key=event.key
+
+const key=event.key;
+
 if(key===codes[index]) {
   console.log(key)
   index++;
   console.log(index)
-  
-  if(index===codes.length){
-    alert("YOU DID IT!");
+  if(index===codes.length)
+  {
+   alert("YOU DID IT!");
     index=0
-    
   }
-  
 }
     else {
       index=0
-      
     }
   }
-  )}
+  )
+}
 
     
     
